067722dc3bec34787ac620fc47c408d01a97e997
David <davidleomendoza300@gmail.com>
2023-10-08 11:35:14 -0500

    Commit como prueba de jenkins

--- JenkinsPrueba/src/Main.java
+++ JenkinsPrueba/src/Main.java
@@ -2,12 +2,7 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        System.out.println((fibonacci(3)));
+        System.out.println( "A commit has been made.");
     }
-    public static int fibonacci(int num){
-         if(num <= 1){
-             return 1;
-         }
-         return fibonacci(num -1) + fibonacci(num - 2);
-    }
+
 }
